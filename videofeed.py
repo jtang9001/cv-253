ebb2a47b3ba06e6a09927e5412db8526f83f1e5f
Jiayi Tang <jiayitang10@gmail.com>
2019-07-09 00:47:09 -0700

    Added more parameters

--- videofeed.py
+++ videofeed.py
@@ -6,10 +6,15 @@ import cv2
 import numpy as np
  
 # initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 20
-rawCapture = PiRGBArray(camera, size=(640, 480))
+res = (640, 480)
+
+camera = PiCamera(
+    #sensor_mode=4,
+    resolution=res,
+    framerate=20)
+#camera.resolution = (640, 480)
+#camera.framerate = 20
+rawCapture = PiRGBArray(camera, size=res)
 
 # allow the camera to warmup
 time.sleep(0.25)
@@ -25,6 +30,14 @@ def auto_canny(image, sigma=0.33):
  
     # return the edged image
     return edged
+
+def auto_canny_upper(image, sigma=0.33):
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+    upper = int(min(255, (1.0 + sigma) * v))
+ 
+    # return the upper bound
+    return upper
  
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -46,10 +59,13 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     circles = cv2.HoughCircles(
         blurred,
         cv2.HOUGH_GRADIENT,
-        1.2,
-        20)
+        dp = 1.5,
+        minDist = 20,
+        param1 = auto_canny_upper(blurred)
+        )
     
     if circles is not None:
+        print(circles)
         for (x,y,r) in circles[0,:]:
             cv2.circle(image, (x,y), r, (0,255,0), 2)
             cv2.circle(image, (x,y), 2, (0,0,255), 3)
